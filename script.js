add37b3c44e38adf4e48cda9303fac267277c458
Maaax02 <massiown@gmail.com>
2022-01-13 14:42:05 +0100

    esercizio + bonus

---
+++ script.js
@@ -0,0 +1,10 @@
+let a = document.getElementById('here');
+new Vue ({
+    el: "#app",
+    data: {
+        message: 'CIAO!',
+        img: "https://image-cdn.beforward.jp/backoffice/uploads/stock/BF12073_1.jpg",
+        name: '',
+        sayHello: 'Ciao ' + name,
+    }
+})
